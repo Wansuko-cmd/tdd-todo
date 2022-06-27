f2248c33f508cfd5c1e09ffa81556fb1afa594c3
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-06-27 20:06:49 +0900

    [add] usecaseにcoroutineへの依存関係を追加

--- core/usecase/build.gradle.kts
+++ core/usecase/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
     implementation(project(":core:domain"))
     implementation(project(":utils"))
+
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
 }
