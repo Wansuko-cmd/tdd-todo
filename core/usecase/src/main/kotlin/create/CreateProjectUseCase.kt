beae9cf7c9368bec918aa2e79c2441ee156850e1
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-06-30 14:29:55 +0900

    [add] CreateProjectの際のエラーハンドリングを追加

--- core/usecase/src/main/kotlin/create/CreateProjectUseCase.kt
+++ core/usecase/src/main/kotlin/create/CreateProjectUseCase.kt
@@ -1,5 +1,6 @@
 package create
 
+import com.wsr.apiresult.mapFailure
 import project.Project
 import project.ProjectDescription
 import project.ProjectRepository
@@ -9,4 +10,9 @@ class CreateProjectUseCase(private val projectRepository: ProjectRepository) {
     suspend operator fun invoke(title: ProjectTitle, description: ProjectDescription) =
         Project(title = title, description = description)
             .let { projectRepository.insert(it) }
+            .mapFailure { CreateProjectUseCaseException.DatabaseException(it.message) }
+}
+
+sealed class CreateProjectUseCaseException : Exception() {
+    data class DatabaseException(override val message: String?) : CreateProjectUseCaseException()
 }
--- core/usecase/src/test/kotlin/create/CreateProjectTest.kt
+++ core/usecase/src/test/kotlin/create/CreateProjectTest.kt
@@ -5,6 +5,8 @@ package create
 import com.wsr.apiresult.ApiResult
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -12,6 +14,7 @@ import org.javalite.test.jspec.JSpec.the
 import project.ProjectDescription
 import project.ProjectRepository
 import project.ProjectTitle
+import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 
@@ -35,5 +38,21 @@ class CreateProjectTest {
             description = ProjectDescription("mockDescription"),
         )
         the(result).shouldBeEqual(ApiResult.Success(Unit))
+    }
+
+    @Test
+    fun Project生成に失敗したとき() = runTest {
+        coEvery { projectRepository.insert(any()) } returns ApiResult.Failure(RepositoryException.DatabaseException("Error"))
+        val result = createProjectUseCase(
+            title = ProjectTitle("mockTitle"),
+            description = ProjectDescription("mockDescription"),
+        )
+        the(result).shouldBeEqual(ApiResult.Failure(CreateProjectUseCaseException.DatabaseException("Error")))
+    }
+
+    @AfterTest
+    fun 関連するメソッド呼び出しの回数の確認() {
+        coVerify(exactly = 1) { projectRepository.insert(any()) }
+        confirmVerified(projectRepository)
     }
 }
