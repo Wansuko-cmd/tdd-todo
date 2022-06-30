c9634e7ed65b14a99f4db1cd677bf822e7d59895
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-06-30 15:07:40 +0900

    [add] Projectの更新コードの失敗時の振る舞いを追加

---
+++ core/usecase/src/test/kotlin/update/project/UpdateProjectFailureTest.kt
@@ -0,0 +1,63 @@
+@file:Suppress("NonAsciiCharacters", "TestFunctionName")
+
+package update.project
+
+import com.wsr.apiresult.ApiResult
+import dto.project.ProjectQueryService
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.javalite.test.jspec.JSpec.the
+import project.*
+import update.UpdateProjectUseCase
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class UpdateProjectFailureTest {
+    private lateinit var updateProjectUseCase: UpdateProjectUseCase
+    @MockK
+    private lateinit var projectQueryService: ProjectQueryService
+    @MockK
+    private lateinit var projectRepository: ProjectRepository
+
+    private val mockProject = Project(
+        id = ProjectId("mockId"),
+        title = ProjectTitle("mockTitle"),
+        description = ProjectDescription("mockDescription"),
+    )
+
+    @BeforeTest
+    fun setup() {
+        MockKAnnotations.init(this)
+        coEvery { projectQueryService.get(mockProject.id) } returns ApiResult.Success(mockProject)
+        coEvery { projectRepository.update(any()) } returns ApiResult.Failure(RepositoryException.DatabaseException("Error"))
+        updateProjectUseCase = UpdateProjectUseCase(projectQueryService, projectRepository)
+    }
+
+    @Test
+    fun 特定のProjectのTitleを更新() = runTest {
+        val result = updateProjectUseCase(projectId = mockProject.id, title = ProjectTitle("newTitle"))
+        the(result).shouldBeEqual(ApiResult.Failure(UseCaseException.DatabaseException("Error")))
+    }
+
+    @Test
+    fun 特定のProjectのDescriptionを更新() = runTest {
+        val result = updateProjectUseCase(projectId = mockProject.id, description = ProjectDescription("newDescription"))
+        the(result).shouldBeEqual(ApiResult.Failure(UseCaseException.DatabaseException("Error")))
+    }
+
+    @AfterTest
+    fun 関連するメソッド呼び出しの回数の確認() {
+        coVerify(exactly = 1) {
+            projectQueryService.get(mockProject.id)
+            projectRepository.update(any())
+        }
+        confirmVerified(projectQueryService, projectRepository)
+    }
+}
