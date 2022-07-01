727f4f4b6f943433b717236d742d891cacef10d9
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-07-01 21:36:02 +0900

    [add] Featureにtasksを保持させる

--- core/domain/src/main/kotlin/feature/Feature.kt
+++ core/domain/src/main/kotlin/feature/Feature.kt
@@ -1,6 +1,7 @@
 package feature
 
 import project.ProjectId
+import task.TaskId
 import java.util.*
 
 data class Feature(
@@ -9,6 +10,7 @@ data class Feature(
     val title: FeatureTitle,
     val description: FeatureDescription,
     val phase: FeaturePhase = FeaturePhase.Todo,
+    val tasks: List<TaskId> = listOf()
 ) {
     fun copyWithPhase(newPhase: FeaturePhase): Feature = copy(phase = newPhase)
 }
