a821bb3cfff9b74ee3c781f2de04ace40b8c5049
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2022-07-01 21:29:31 +0900

    [refactor] Projectのテストを分割

---
+++ core/domain/src/test/kotlin/project/ChangeProjectPropertyTest.kt
@@ -0,0 +1,26 @@
+@file:Suppress("NonAsciiCharacters", "TestFunctionName")
+
+package project
+
+import org.javalite.test.jspec.JSpec.the
+import kotlin.test.Test
+
+class ChangeProjectPropertyTest {
+
+    private val mockProject = Project.create(
+        title = ProjectTitle("mockProjectTitle"),
+        description = ProjectDescription("mockProjectDescription"),
+    )
+
+    @Test
+    fun ProjectのTitleを書き換えたコピーを作成() {
+        val copiedProject = mockProject.changeTitle(ProjectTitle("copiedProjectTitle"))
+        the(copiedProject.title).shouldBeEqual(ProjectTitle("copiedProjectTitle"))
+    }
+
+    @Test
+    fun ProjectのDescriptionを書き換えたコピーを作成() {
+        val copiedProject = mockProject.changeDescription( ProjectDescription("copiedProjectDescription"))
+        the(copiedProject.description).shouldBeEqual(ProjectDescription("copiedProjectDescription"))
+    }
+}
--- core/domain/src/test/kotlin/project/GenerateProjectTest.kt
+++ core/domain/src/test/kotlin/project/GenerateProjectTest.kt
@@ -10,7 +10,7 @@ import java.util.*
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 
-class ProjectTest {
+class GenerateProjectTest {
     @BeforeTest
     fun setup() {
         mockkStatic(UUID::class)
@@ -24,6 +24,7 @@ class ProjectTest {
             title = ProjectTitle("mockProjectTitle"),
             description = ProjectDescription("mockProjectDescription"),
         )
+
         the(mockProject.id).shouldBeEqual(ProjectId("mockUUID"))
         the(mockProject.title).shouldBeEqual(ProjectTitle("mockProjectTitle"))
         the(mockProject.description).shouldBeEqual(ProjectDescription("mockProjectDescription"))
@@ -33,34 +34,15 @@ class ProjectTest {
     @Test
     fun Projectを再生成() {
         val mockProject = Project.reconstruct(
-            id = ProjectId("mockUUID"),
+            id = ProjectId("mockProjectId"),
             title = ProjectTitle("mockProjectTitle"),
             description = ProjectDescription("mockProjectDescription"),
             features = List(3) { index -> FeatureId("mockFeatureId$index") }
         )
-        the(mockProject.id).shouldBeEqual(ProjectId("mockUUID"))
+
+        the(mockProject.id).shouldBeEqual(ProjectId("mockProjectId"))
         the(mockProject.title).shouldBeEqual(ProjectTitle("mockProjectTitle"))
         the(mockProject.description).shouldBeEqual(ProjectDescription("mockProjectDescription"))
         the(mockProject.features).shouldBeEqual(List(3) { index -> FeatureId("mockFeatureId$index") })
-    }
-
-    @Test
-    fun ProjectのTitleを書き換えたコピーを作成() {
-        val mockProject = Project.create(
-            title = ProjectTitle("mockProjectTitle"),
-            description = ProjectDescription("mockProjectDescription"),
-        )
-        val copiedProject = mockProject.changeTitle(title = ProjectTitle("copiedProjectTitle"))
-        the(copiedProject.title).shouldBeEqual(ProjectTitle("copiedProjectTitle"))
-    }
-
-    @Test
-    fun ProjectのDescriptionを書き換えたコピーを作成() {
-        val mockProject = Project.create(
-            title = ProjectTitle("mockProjectTitle"),
-            description = ProjectDescription("mockProjectDescription"),
-        )
-        val copiedProject = mockProject.changeDescription(description = ProjectDescription("copiedProjectDescription"))
-        the(copiedProject.description).shouldBeEqual(ProjectDescription("copiedProjectDescription"))
     }
 }
